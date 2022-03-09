cc67938c33bad172ba2ab07009bb2b8e19fb1fe0
Joshua Beedle <josh.beedle@gmail.com>
2022-03-09 21:41:02 +0000

    completed 'send email' functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -11,23 +11,51 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email() {
-
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  // Store HTML elements
+  const recipientsEl = document.querySelector('#compose-recipients');
+  const subjectEl = document.querySelector('#compose-subject');
+  const bodyEl = document.querySelector('#compose-body');
+  const formEl = document.querySelector('#compose-form');
+
   // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  recipientsEl.value = '';
+  subjectEl.value = '';
+  bodyEl.value = '';
+
+  // Event Handler for sending Emails
+  formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+    send_email(recipientsEl.value, subjectEl.value, bodyEl.value);
+    return false;
+  });
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_email(recipients, subject, body) {
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+    load_mailbox('sent');
+  })
 }
